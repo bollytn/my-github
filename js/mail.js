f99fdb1c58c2b16c366a9d8659c56c6353dd0243
bollytn <ps3online85@gmail.com>
2024-08-08 13:35:18 +0100

    delete sendMail

--- js/mail.js
+++ js/mail.js
@@ -3,9 +3,6 @@ function sendMail() {
         name : document.getElementById("name").value ,
         email : document.getElementById("email").value ,
         mobile : document.getElementById("mobile").value,
-        message : document.getElementById("message").value,
-        select_choice : document.getElementById("choice").value,
-        choice_id : select_choice.option[select_choice.selectedIndex].value, 
     };
 const serviceID = "service_kjr0def";
 const templateID = "template_1pvca8t";
@@ -16,8 +13,6 @@ emailjs.send(serviceID,templateID,params)
         document.getElementById("name").value = "";
         document.getElementById("email").value = "";
         document.getElementById("mobile").value = "";
-        document.getElementById("message").value = "";
-        choice.getElementById("choice").value = "";
         console.log(res);
         alert("votre message est envoyer avec succès")
     })
