77d9fa6e4a5d3e2140baaf6730b7cf71a78061e6
bollytn <ps3online85@gmail.com>
2024-08-08 13:25:13 +0100

    completer la fonction  sendMail

---
+++ js/mail.js
@@ -0,0 +1,27 @@
+function sendMail() {
+    var params = {
+        name : document.getElementById("name").value ,
+        email : document.getElementById("email").value ,
+        mobile : document.getElementById("mobile").value,
+        message : document.getElementById("message").value,
+        select_choice : document.getElementById("choice").value,
+        choice_id : select_choice.option[select_choice.selectedIndex].value, 
+    };
+const serviceID = "service_kjr0def";
+const templateID = "template_1pvca8t";
+
+emailjs.send(serviceID,templateID,params)
+.then(
+    res => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("mobile").value = "";
+        document.getElementById("message").value = "";
+        choice.getElementById("choice").value = "";
+        console.log(res);
+        alert("votre message est envoyer avec succès")
+    })
+.catch((err) => console.log(err));
+}
+
+    
