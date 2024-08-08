559aa81bc94ad7e823b64d3d489ed06c9c93a9d3
bollytn <ps3online85@gmail.com>
2024-08-08 16:46:17 +0100

    modifier template id

--- js/mail.js
+++ js/mail.js
@@ -3,9 +3,10 @@ function sendMail() {
         name : document.getElementById("name").value ,
         email : document.getElementById("email").value ,
         mobile : document.getElementById("mobile").value,
+        message : document.getElementById("message").value,
     };
 const serviceID = "service_kjr0def";
-const templateID = "template_1pvca8t";
+const templateID = "template_zy2gqqh";
 
 emailjs.send(serviceID,templateID,params)
 .then(
@@ -13,6 +14,7 @@ emailjs.send(serviceID,templateID,params)
         document.getElementById("name").value = "";
         document.getElementById("email").value = "";
         document.getElementById("mobile").value = "";
+        document.getElementById("message").value = "";
         console.log(res);
         alert("votre message est envoyer avec succès")
     })
